f04110f585089d8c96b1f7874b4192febe9acddd
India Cornell <indiaccornell@gmail.com>
2023-06-02 15:24:51 -0700

    updating hello.js

--- hello.js
+++ hello.js
@@ -14,3 +14,19 @@ sayHello("Ferdinand");
 //sayHello is one of two main varieties of functions://
 //One that produces a side effect, as in it does something.//
 //One that produces a result, as in it calculates and returns a value we can use in further code.//
+//console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+//return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+//returning a value of a function with console.log - same result as first option
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
