a4fbe725178e606b531c8389287f80b91a04ea83
India Cornell <indiaccornell@gmail.com>
2023-06-04 13:56:26 -0700

    updating packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -11,3 +11,15 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 packingList[3];
+
+//for loops - start at 0 not 1, as does indexing of arrays and a good option to iterate over items in an array. 
+
+//updated code to use a while loop:
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+//note this works the same, but spaces out the logic 
