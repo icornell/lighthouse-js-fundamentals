e1d5524d84f48243673f617d512447981bb17489
India Cornell <indiaccornell@gmail.com>
2023-06-04 13:57:17 -0700

    adding ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer"); //add root beer to array without modifing the original
+console.log(iceCreamFlavours); 
+//get first flavour of ice cream
+console.log(iceCreamFlavours[0]);
+//get the last flavour of ice cream
+console.log(iceCreamFlavours[5])
+//caluate number of ice cream flavours
+console.log(iceCreamFlavours.length);
+
+
